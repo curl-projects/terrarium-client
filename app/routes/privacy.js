99b188b2dd31a1cfef524b8f130b133390126638
finnianmacken <finn@minerva.kgi.edu>
2022-11-11 10:32:11 -0300

    updated dockerfile

---
+++ app/routes/privacy.js
@@ -0,0 +1,8 @@
+export default function Privacy(){
+  return(
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: "center"}}>
+      <h1>Privacy Policy</h1>
+      <p>This website records your Google Id and email so that we can connect your Notion workspace to your Discord</p>
+    </div>
+  )
+}
