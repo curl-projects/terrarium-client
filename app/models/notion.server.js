ef7b550a016f3df7921060a2bfd06089b348ea2c
finnianmacken <finn@minerva.kgi.edu>
2022-11-13 15:26:41 -0300

    Fixed Notion auth

--- app/models/notion.server.js
+++ app/models/notion.server.js
@@ -2,21 +2,42 @@ import axios from 'axios';
 import { db } from "~/models/db.server"
 
 export async function authenticateNotionCode(code){
-  const config = {
-    method: "post",
-    url: "https://api.notion.com/v1/oauth/token",
-    auth: {
-      username: process.env.NOTION_CLIENT_ID,
-      password: process.env.NOTION_CLIENT_SECRET
-    },
-    data: {
-      grant_type: "authorization_code",
-      code: code,
-    },
-    headers: { "Content-Type": "application/json"}
-  }
 
-  const response = await axios(config)
+  const url = "https://api.notion.com/v1/oauth/token"
+  const data = {
+    "grant_type": "authorization_code",
+    "code": code,
+    "redirect_uri": "http://localhost:3000/auth/notion"}
+
+  const authToken = Buffer.from(
+    `${process.env.NOTION_CLIENT_ID}:${process.env.NOTION_CLIENT_SECRET}`
+  ).toString("base64");
+
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Authorization": `Basic ${authToken}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+
+  // const config = {
+  //   method: "post",
+  //   url: "https://api.notion.com/v1/oauth/token",
+  //   auth: {
+  //     username: process.env.NOTION_CLIENT_ID,
+  //     password: process.env.NOTION_CLIENT_SECRET
+  //   },
+  //   data: {
+  //     grant_type: "authorization_code",
+  //     code: code,
+  //   },
+  //   headers: { "Content-Type": "application/json"}
+  // }
+  //
+  // const response = await axios(config)
 
   const jsonResponse = await response.json()
   return jsonResponse
@@ -47,4 +68,8 @@ export async function createNotionAuth(notionData, userId){
   })
 
   return notionAuth
+}
+
+async function verifyNotionAccess(discordUsername){
+
 }
