052455a895ea45e81b0b6b475b37b5bb29204f0a
finnianmacken <finn@minerva.kgi.edu>
2022-11-19 10:08:39 -0300

    Finalised integrations

--- app/routes/dashboard.jsx
+++ app/routes/dashboard.jsx
@@ -73,8 +73,10 @@ export default function Dashboard(){
   const actionData = useActionData();
   const transition = useTransition();
   const [notionIsAuthenticated, setNotionIsAuthenticated] = useState(false)
+  const [discordIsAuthenticated, setDiscordIsAuthenticated] = useState(false)
   const [discordUpdate, setDiscordUpdate] = useState(false)
   const [discordIsUpdated, setDiscordIsUpdated] = useState(false)
+
 
   useEffect(()=>{
     if(loaderData.notionAuth.length === 0){
@@ -82,6 +84,13 @@ export default function Dashboard(){
     }
     else{
       setNotionIsAuthenticated(true)
+    }
+
+    if(loaderData.discordUsers.length === 0){
+      setDiscordIsAuthenticated(false)
+    }
+    else{
+      setDiscordIsAuthenticated(true)
     }
   }, [loaderData])
 
@@ -108,17 +117,24 @@ export default function Dashboard(){
                 <img className='integrationIcon' src={notionIcon}></img>
               </div>
               <div style={{flex: 1}}/>
-              <button className={notionIsAuthenticated ? 'connectedButton' : 'connectButton'} onClick={()=> window.open('https://api.notion.com/v1/oauth/authorize?client_id=d2e58919-d704-42a9-8638-c8c92806d68c&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fnotion', "_self")}>
-                {notionIsAuthenticated
-                  ? <p style={{fontSize: "18px", color: 'rgb(94, 160, 85)'}}>Connected</p>
-                  : <p style={{fontSize: "18px"}}>Connect</p>}
-              </button>
+              <div className='buttonRowWrapper'>
+                {notionIsAuthenticated &&
+                <button className='connectButton' onClick={()=> window.open('https://api.notion.com/v1/oauth/authorize?client_id=d2e58919-d704-42a9-8638-c8c92806d68c&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fnotion', "_self")}>
+                  <p style={{fontSize: "18px"}}>Update</p>
+                </button>
+                }
+                <button className={notionIsAuthenticated ? 'connectedButton' : 'connectButton'} onClick={function(){if(!notionIsAuthenticated){window.open('https://api.notion.com/v1/oauth/authorize?client_id=d2e58919-d704-42a9-8638-c8c92806d68c&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fnotion', "_self")}}}>
+                  {notionIsAuthenticated
+                    ? <p style={{fontSize: "18px", color: 'rgb(94, 160, 85)'}}>Connected</p>
+                    : <p style={{fontSize: "18px"}}>Connect</p>}
+                </button>
+              </div>
             </div>
             <div className='serviceNameWrapper'>
               <p style={{fontSize: "24px"}}>Notion</p>
             </div>
             <div style={{flex: 1}}>
-              <p>Connect your Notion workspace to sync your Discord bug reports</p>
+              <p>Connect your Notion workspace to sync your Discord bug reports. The integration looks for an anchor page called Terrarium, so make sure you've created it and we have access to it!</p>
             </div>
           </div>
         </div>
@@ -130,11 +146,16 @@ export default function Dashboard(){
                 <img className='integrationIcon' src={discordIcon}></img>
               </div>
               <div style={{flex: 1}}/>
-              <button className={discordIsUpdated ? "connectedButton" : 'connectButton'} onClick={()=>setDiscordUpdate(prevState => !prevState)}>
-                {discordIsUpdated
-                  ? <p style={{fontSize: "18px", color: 'rgb(94, 160, 85)'}}>Updated</p>
-                  : <p style={{fontSize: "18px"}}>{discordUpdate ? "Cancel" : "Update"}</p>}
-              </button>
+              <div className='buttonRowWrapper'>
+                <button className='connectButton' onClick={()=>setDiscordUpdate(prevState => !prevState)}>
+                    <p style={{fontSize: "18px"}}>{discordUpdate ? "Cancel" : "Update"}</p>
+                </button>
+                <button className={discordIsAuthenticated ? 'connectedButton' : 'connectButton'}>
+                  {discordIsAuthenticated
+                    ? <p style={{fontSize: "18px", color: 'rgb(94, 160, 85)'}}>Connected</p>
+                    : <p style={{fontSize: "18px"}}>Connect</p>}
+                </button>
+              </div>
             </div>
             <div className='serviceNameWrapper'>
               <p style={{fontSize: "24px"}}>Discord</p>
