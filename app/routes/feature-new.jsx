b9207f692a91db85c65f749a4703d136ad50e658
finnianmacken <finn@minerva.kgi.edu>
2023-05-16 20:32:42 -0700

    roadmap kanban initial scaffold

---
+++ app/routes/feature-new.jsx
@@ -0,0 +1,8 @@
+export default function New(){
+    return(
+        <div className="featureScaffold">
+            <div className="featureTitle"></div>
+            <div className='featureTitle'></div>
+        </div>
+    )
+}
