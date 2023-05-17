0b3958703ba4beeacfaf68f067a0bfabcc39e3fe
finnianmacken <finn@minerva.kgi.edu>
2023-05-16 21:06:37 -0700

    fixed roadmap header

--- app/components/Roadmap/RoadmapColumn.js
+++ app/components/Roadmap/RoadmapColumn.js
@@ -10,33 +10,39 @@ export default function KanbanColumn(props){
       className='kanbanColumnOuter'
       key={props.columnId}
     >
-    <Droppable droppableId={props.columnId} key={props.columnId}>
-        {(provided, snapshot) => {
-        return (
-            <div
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-            style={{
-                padding: "30px",
-                width: '100%',
-                height: "100%",
-                overflow: "scroll",
+        <div className='kanbanColumnHeader'>
+            <div className="kanbanColumnHeaderTextWrapper"><p className="kanbanColumnHeaderText">Roadmap</p></div>
+            <div className='messageNumberWrapper'><p className="messageNumber">3</p></div>
+            <div style={{flex: 1}} />
+            <div className='addMessageWrapper'><p className='addMessage'>+</p></div>
+        </div>
+        <Droppable droppableId={props.columnId} key={props.columnId}>
+            {(provided, snapshot) => {
+            return (
+                <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={{
+                    padding: "30px",
+                    width: '100%',
+                    height: "100%",
+                    overflow: "scroll",
+                }}
+                >
+                    {props.column.items && props.column.items.map((item, index) => {
+                    return (
+                    <RoadmapCard key={index}
+                                    item={item}
+                                    index={index}
+                                    updateHoveredData={props.updateHoveredData}
+                                    />
+                    );
+                })}
+                {provided.placeholder}
+                </div>
+            );
             }}
-            >
-                {props.column.items && props.column.items.map((item, index) => {
-                return (
-                <RoadmapCard key={index}
-                                item={item}
-                                index={index}
-                                updateHoveredData={props.updateHoveredData}
-                                />
-                );
-            })}
-            {provided.placeholder}
-            </div>
-        );
-        }}
-    </Droppable>
+        </Droppable>
         <div className='roadmapControlPanel'>
             <Form method='post'>
                 <input type="hidden" name='actionType' value='create'/>
