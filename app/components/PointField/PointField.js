693134604179813722a6b24a11a3e3a2c69b8d4f
finnianmacken <finn@minerva.kgi.edu>
2023-05-18 11:52:46 -0700

    fixed cluster rendering

--- app/components/PointField/PointField.js
+++ app/components/PointField/PointField.js
@@ -2,7 +2,9 @@ import { useD3 } from '~/utils/useD3';
 import { useWindowSize } from "~/utils/useWindowSize";
 import React, {useEffect, useRef, useState, useCallback, useLayoutEffect } from 'react';
 import * as d3 from 'd3';
+import { teardown } from '@mui/utils/useIsFocusVisible';
 
+// DISPLAY CONTROL MANAGEMENT
 function usePrevious(value) {
   const ref = useRef();
   useEffect(() => {
@@ -15,19 +17,23 @@ export default function PointField({ data, clusters, searchResults, filterBrushe
                                    resetBrushFilter, zoomObject, setZoomObject,
                                    displayControl, resetZoomedData, headerCollapsed}) {
 
-  // the weird domains create padding around the svg
+  // COMPONENT STRUCTURE
   const xDomain = [-0.05, 1.05]
   const yDomain = [-0.10, 1.15]
 
   const [containerHeight, setContainerHeight] = useState(0)  
   const [containerWidth, setContainerWidth] = useState(0)  
 
-  // const xDomain = [0, 1]
-  // const yDomain = [0, 1]
-  const [windowWidth, windowHeight] = useWindowSize();
+  const measuredRef = useCallback(node => {
+    if (node !== null) {
+      setContainerHeight(node.getBoundingClientRect().height); 
+      setContainerWidth(node.getBoundingClientRect().width);
+    }
+  }, []);
 
   const prevDisplayControl = usePrevious(displayControl)
 
+//   DATA RENDERING
   useEffect(()=>{
     var x = d3.scaleLinear()
     .domain(xDomain)
@@ -38,59 +44,97 @@ export default function PointField({ data, clusters, searchResults, filterBrushe
       .domain(yDomain)
       .range([ref.current.clientHeight, 0]);
 
-    function renderData(){
+    // UTILITY FUNCTIONS
+    // // ANIMATE DATA
+    function animateData(){
         d3.selectAll(".frNode")
           .data(data)
           .transition()
-             .duration(500)
+             .duration(1000)
              .ease(d3.easeCubicInOut)
              .attr('cx', d => x(d.xDim))
              .attr('cy', d => y(d.yDim))
     }
 
-    renderData()
+    // // RENDER CLUSTERS
+    function renderClusters(){
 
-  }, [data])
+        const clusterLayer = d3.select("#canvas-svg").insert("g").attr("id", "clusterlayer")
 
+        const clusterNodes = clusterLayer.selectAll('dot')
+            .data(clusters)
+            .join('circle')
+              .attr('class', "clusterNode")
+              .attr("r", 0)
+              .style('opacity', 0)
+              .attr('cx', d => x(d.xDim))
+              .attr('cy', d => y(d.yDim))
+              .attr('fill', "rgba(119, 153, 141, 0.7)")
+              .on("click", function(e){
+                console.log("DATA", e.target.__data__)
+                setZoomObject({"id": e.target.__data__.id, "type": e.target.__data__.type})
+              })
+              .transition(1000)
+                .delay(500)
+                .attr("r", 35)
+                .style('opacity', 0.2)
+      }
+
+    // // TEAR DOWN CLUSTERS
+    function tearDownClusters(){
+        // TEAR DOWN CLUSTER BLOBS
+        d3.select(ref.current)
+          .selectAll(".clusterNode")
+          .transition()
+          .duration(500)
+          .attr('r', 0)
+          .remove()
+        
+          d3.select("#cluster-layer").remove()
+      }
+
+    // TEAR DOWN ALL DATA OBJECTS
+    tearDownClusters()
+
+    // RENDER ALL OBJECTS
+    animateData()
+    displayControl.clusters && renderClusters()
+
+  }, [data, displayControl])
+
+// DATA INSTANTIATION
   const ref = useD3(
     (svg) => {
-        // rerender the canvas on data, hieght, or width change
+    // rerender the canvas on data, hieght, or width change
     d3.select("#canvas-svg").selectAll("*").remove();
 
-      const margin = {top: 0, right: 0, bottom: 0, left: 0};
+    // X-AXIS
+    var x = d3.scaleLinear()
+    .domain(xDomain)
+    .range([0, containerWidth]);
 
-      // X-AXIS
-      var x = d3.scaleLinear()
-        .domain(xDomain)
-        .range([0, containerWidth]);
+    // Y-AXIS
+    var y = d3.scaleLinear()
+    .domain(yDomain)
+    .range([containerHeight, 0]);
 
-      // Y-AXIS
-      var y = d3.scaleLinear()
-        .domain(yDomain)
-        .range([containerHeight, 0]);
-
-      const dots = svg.insert("g").attr('id', 'dotlayer')
-        .selectAll("dot")
-        .data(data)
-        .join('circle')
-          .attr('id', d => `fr-${d.fr_id}`)
-          .attr('class', 'frNode')
-          .attr('cx', d => x(d.xDim))
-          .attr('cy', d => y(d.yDim))
-          .attr('r', 5)
-          .attr('fill', "rgba(119, 153, 141, 0.5)")
-          .attr("stroke", 'rgba(119, 153, 141, 1)')
-          .attr("stroke-width", 2)
+    const dots = svg.insert("g").attr('id', 'dotlayer')
+    .selectAll("dot")
+    .data(data)
+    .join('circle')
+        .attr('id', d => `fr-${d.fr_id}`)
+        .attr('class', 'frNode')
+        .attr('cx', d => x(d.xDim))
+        .attr('cy', d => y(d.yDim))
+        .attr('r', 5)
+        .attr('fill', "rgba(119, 153, 141, 0.5)")
+        .attr("stroke", 'rgba(119, 153, 141, 1)')
+        .attr("stroke-width", 2)
+    
     },
     [data.length, containerHeight, containerWidth]
-  );
+);
 
-  const measuredRef = useCallback(node => {
-    if (node !== null) {
-      setContainerHeight(node.getBoundingClientRect().height); 
-      setContainerWidth(node.getBoundingClientRect().width);
-    }
-  }, []);
 
   useEffect(()=>{
     console.log("CONTAINER HEIGHT, CONTAINER WIDTH", containerHeight, containerWidth)
